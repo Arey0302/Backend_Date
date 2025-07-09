665314e8b8eb98fc3fdcfac4d031f6b6311feac5
Nguyen Duy Linh <nguyenduylinh0199@gmail.com>
2025-07-10 01:03:12 +0700

    Initial commit

---
+++ src/components/Heart/Heart.tsx
@@ -0,0 +1,51 @@
+import { useRef } from "react";
+import "./HeartSlider.css"; // Import your CSS file for styles
+
+const HeartSlider = ({ value, onChange }: { value: number, onChange: (val: number) => void }) => {
+  const containerRef = useRef<HTMLDivElement>(null);
+  const animations = ["float-1", "float-2", "float-3", "float-4"];
+  const animationDuration = 2000;
+
+  const handleInput = (e: any) => {
+    const newValue = Number(e.target.value);
+    onChange(newValue);
+
+    const randomAnimation =
+      animations[Math.floor(Math.random() * animations.length)];
+
+    const heart = document.createElement("div");
+    heart.classList.add("heart");
+    heart.textContent = "💙";
+    heart.style.animationName = randomAnimation;
+    heart.style.animationDuration = `${animationDuration}ms`;
+    heart.style.filter = `hue-rotate(${newValue * 3.6}deg)`;
+    heart.style.left = `${newValue}%`;
+
+    if (containerRef.current) {
+      containerRef.current.appendChild(heart);
+    }
+
+    heart.onanimationend = ({ target }) => {
+      if (target) {
+        (target as HTMLElement).remove();
+      }
+    };
+  };
+
+  return (
+    <>
+      <div className="slider container" ref={containerRef}>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          value={value}
+          style={{ width: "100%" }}
+          onChange={handleInput}
+        />
+      </div>
+    </>
+  );
+};
+
+export default HeartSlider;
